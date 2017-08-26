addf903538b3af46452567bc28e18c52936eb538
Rosalyn <r928wong@gmail.com>
2017-08-26 13:58:31 -0400

    print response

--- datethe6ix.java
+++ datethe6ix.java
@@ -1,5 +1,12 @@
-import java.net.*;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Scanner;
 
 //access token: 737721599747740|qANITqJEuZ7d-0pFTjEtznP9Sjc
 public class datethe6ix{
@@ -16,14 +23,20 @@ public class datethe6ix{
 		connection.setRequestProperty("redirect_uri", "http://www.internest.ca");
 		try{
 			InputStream response=connection.getInputStream();
+				try (Scanner scanner=new Scanner(response)){
+		String responseBody=scanner.useDelimiter("\\A").next();
+		System.out.println(responseBody);
+	}
 		}
 		catch(IOException e){
 			System.out.print("InputStream error");
 		}
 	//HttpURLConnection con=(HttpURLConnection) url.openConnection();
 	//con.setRequestMethod("GET");
-
-		
+	
+	
 
 	}
+
+
 }
