0ca0be428674e6e24c8b4550538599b94ca57e20
Rosalyn <r928wong@gmail.com>
2017-08-26 19:26:41 -0400

    json file

--- datethe6ix.java
+++ datethe6ix.java
@@ -7,12 +7,16 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.Scanner;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import org.json.*;
+
+//javac -cp /usr/share/java/json-20160212.jar JsonParser.java
+// java -cp ".;java-json.jar" datethe6ix
 
 //access token: 737721599747740|qANITqJEuZ7d-0pFTjEtznP9Sjc
 public class datethe6ix{
 	public static void main(String[] args){
-		String url="https://graph.facebook.com/search?q=music&type=event&access_token=EAACEdEose0cBAKfqZCX5kRDZAV0vUKeIfBoBDXlPMoo1jb3lAOJhgizQ5pWVOpNSZAGILgrNlDMG69Bhubpg6Qt9TrX7HqfhZAoaxiqI6Hdb8kaH2gJu0PLC89Cx7c1qNKkZAaH2W34mxyRdZAB8oVmRxL2JsD8htm2GIL3aLBGYCzxPpuR3VkzZAh08PreTybmomkLYTk1YAZDZD";
+		String url="https://graph.facebook.com/search?q=music&type=event&access_token=EAACEdEose0cBAPuXnTbZCRmmuGcKfZB8GjhEuuqRIF1a4AE0LIlYBOnbZBwubbZAE2MvIx9YdcpZBQqs7KuwZCXqMZBZCZBO9AzfWVM3LTr1rFMQC2K7OdqKDpnwjL1lDgBEWhmzsHlNlU37YzL94JsIzQZCYjruwshxjwRZCEjl6xkZAJwfFvrUyNe9IjUVf13yluTRTXun0TDZCXQZDZD";
 		URLConnection connection=null;
 		
 		try{
@@ -28,18 +32,28 @@ public class datethe6ix{
 
 			try (Scanner scanner=new Scanner(response)){
 				String responseBody=scanner.useDelimiter("\\A").next();
+				JSONObject resobj=null;
 				try{
-					PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
-					writer.println(responseBody);
+					resobj=new JSONObject(responseBody);
+					//System.out.println(resobj);
+				}
+				catch(JSONException e){
+					System.out.println(e);
+				}
 
-					writer.close();
+				try{
+					FileWriter writer = new FileWriter("6ix.txt");
+					writer.write(resobj.toString());
+					//writer.close();
 				} catch (IOException e) {
+					System.out.println("Stop!");
 					System.out.println(e);
 				}
 				System.out.println("Done!");
 			}
 		}
 		catch(IOException e){
+			System.out.println("No");
 			System.out.print(e);
 		}
 	//HttpURLConnection con=(HttpURLConnection) url.openConnection();
