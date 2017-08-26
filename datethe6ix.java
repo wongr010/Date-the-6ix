44438948d7216a5f055ad3736ef8b2bda1af83e9
Rosalyn <r928wong@gmail.com>
2017-08-26 15:18:52 -0400

    event dump

--- datethe6ix.java
+++ datethe6ix.java
@@ -7,11 +7,12 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.Scanner;
+import java.io.PrintWriter;
 
 //access token: 737721599747740|qANITqJEuZ7d-0pFTjEtznP9Sjc
 public class datethe6ix{
 	public static void main(String[] args){
-		String url="https://facebook.com/v2.10/dialog/oauth";
+		String url="https://graph.facebook.com/search?q=music&type=event&access_token=EAACEdEose0cBAKfqZCX5kRDZAV0vUKeIfBoBDXlPMoo1jb3lAOJhgizQ5pWVOpNSZAGILgrNlDMG69Bhubpg6Qt9TrX7HqfhZAoaxiqI6Hdb8kaH2gJu0PLC89Cx7c1qNKkZAaH2W34mxyRdZAB8oVmRxL2JsD8htm2GIL3aLBGYCzxPpuR3VkzZAh08PreTybmomkLYTk1YAZDZD";
 		URLConnection connection=null;
 		
 		try{
@@ -19,23 +20,32 @@ public class datethe6ix{
 		}catch(IOException e){
 			System.out.print("connection error!");
 		}
-		connection.setRequestProperty("client_id", "737721599747740");
-		connection.setRequestProperty("redirect_uri", "http://www.internest.ca");
-		connection.setRequestProperty("response_type", "token");
+		//connection.setRequestProperty("q", "food");
+		//connection.setRequestProperty("type", "event");
+		//connection.setRequestProperty("access_token", "EAACEdEose0cBAKfqZCX5kRDZAV0vUKeIfBoBDXlPMoo1jb3lAOJhgizQ5pWVOpNSZAGILgrNlDMG69Bhubpg6Qt9TrX7HqfhZAoaxiqI6Hdb8kaH2gJu0PLC89Cx7c1qNKkZAaH2W34mxyRdZAB8oVmRxL2JsD8htm2GIL3aLBGYCzxPpuR3VkzZAh08PreTybmomkLYTk1YAZDZD");
 		try{
 			InputStream response=connection.getInputStream();
-				try (Scanner scanner=new Scanner(response)){
-		String responseBody=scanner.useDelimiter("\\A").next();
-		System.out.println(responseBody);
-	}
+
+			try (Scanner scanner=new Scanner(response)){
+				String responseBody=scanner.useDelimiter("\\A").next();
+				try{
+					PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
+					writer.println(responseBody);
+
+					writer.close();
+				} catch (IOException e) {
+					System.out.println(e);
+				}
+				System.out.println("Done!");
+			}
 		}
 		catch(IOException e){
-			System.out.print("InputStream error");
+			System.out.print(e);
 		}
 	//HttpURLConnection con=(HttpURLConnection) url.openConnection();
 	//con.setRequestMethod("GET");
-	
-	
+
+
 
 	}
 
